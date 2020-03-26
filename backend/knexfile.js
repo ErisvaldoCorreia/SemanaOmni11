e3a8e3927c039af0b1bc8c73f8db48b66493cf95
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-03-25 23:45:03 -0300

    feat(backend-database): create Migrations for the database in project

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,7 +4,11 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './src/database/db.sqlite'
-    }
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true
   },
 
   staging: {

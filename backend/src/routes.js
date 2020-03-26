15ec98e45aeffab148cc5b2a9b078e17f44dbac5
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-03-26 02:25:24 -0300

    refactor(routes-ong): criando os controllers para as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,9 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+
 const routes = express.Router();
 
-routes.get('/api/ongs', async (req, res) => {
-  const ongs = await connection('ongs').select('*');
-  return res.json(ongs);
-});
-
-routes.post('/api/ongs', async (req, res) => {
-  const {name, email, whatsapp, city, uf} = req.body;
-  const id = crypto.randomBytes(4).toString('HEX');
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  })
-  return res.json({id});
-});
+routes.get('/api/ongs', OngController.list);
+routes.post('/api/ongs', OngController.create);
 
 module.exports = routes;

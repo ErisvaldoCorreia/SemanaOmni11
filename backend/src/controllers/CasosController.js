7bd09bbe315601b53029fd701c20430dfdd0a24e
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-03-26 17:32:58 -0300

    feat(routes-session): create session route and filter route list all adding casos on return

--- backend/src/controllers/CasosController.js
+++ backend/src/controllers/CasosController.js
@@ -2,9 +2,34 @@ const connection = require('../database/connection');
 
 module.exports = {
     async list(req, res) {
-        const casos = await connection('casos').select('*');
+        const { page = 1 } = req.query;
+        const [count] = await connection('casos').count();
+        const casos = await connection('casos')
+            .join('ongs', 'ongs.id', '=', 'casos.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'casos.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
+
+        res.header('X-Total-Count', count['count(*)'])
         return res.json(casos);
     },
+
+    async listByOng(req, res) {
+        const ong_id = req.headers.authorization;
+        const casos = await connection('casos')
+            .where('ong_id', ong_id)
+            .select('*');
+
+        return res.json(casos);
+    },
+
     async create(req, res) {
         const { title, description, value } = req.body;
         const ong_id = req.headers.authorization;
@@ -34,5 +59,19 @@ module.exports = {
 
         await connection('casos').where('id', id).delete();
         return res.status(204).send();
+    },
+
+    async session(req, res) {
+        const { id } = req.body;
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if(!ong) {
+            return res.status(400).json({ error: 'Not ONG found with this id' });
+        };
+
+        return res.json(ong);
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,8 +7,12 @@ const routes = express.Router();
 routes.get('/api/ongs', OngController.list);
 routes.post('/api/ongs', OngController.create);
 
+routes.post('/api/session', CasosController.session);
+
 routes.get('/api/casos', CasosController.list);
-routes.post('/api/casos', CasosController.create);
-routes.delete('/api/casos/:id', CasosController.remove);
+
+routes.get('/api/profile/casos', CasosController.listByOng);
+routes.post('/api/profile/casos', CasosController.create);
+routes.delete('/api/profile/casos/:id', CasosController.remove);
 
 module.exports = routes;

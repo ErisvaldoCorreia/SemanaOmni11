0f43792f02686ff762cbb4b280339341f4925c2d
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-03-27 23:06:29 -0300

    estilização header page main

--- mobile/src/pages/main/index.js
+++ mobile/src/pages/main/index.js
@@ -8,20 +8,46 @@ import Logo from './../../assets/logo.png';
 export default function Main() {
   return (
     <SafeAreaView style={styles.container}>
-        <View>
+        <View style={styles.containerArea}>
             <Image source={Logo} />
-            <Text>
-                Total de <Text>0 casos</Text>
+            <Text style={styles.header}>
+                Total de <Text style={styles.headerBold}>0 casos</Text>
             </Text>
         </View>
-        <Text>Seja Bem vindo!</Text>
-        <Text>Escolha um caso e seja um heroi no dia de hoje!</Text>
+        <Text style={styles.title}>Bem-vindo!</Text>
+        <Text style={styles.titleDesc}>Escolha um caso e seja um heroi de hoje!</Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: Constants.statusBarHeight + 10
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingTop: Constants.statusBarHeight + 20
+    },
+    containerArea: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    header: {
+        fontSize: 15,
+        color: '#737380'
+    },
+    headerBold: {
+        fontWeight: 'bold'
+    },
+    title: {
+        fontSize: 30,
+        color: '#13131a',
+        marginBottom: 16,
+        marginTop: 34,
+        fontWeight: 'bold'
+    },
+    titleDesc: {
+        fontSize: 15,
+        color: '#737380',
+        lineHeight: 24
     }
 })
